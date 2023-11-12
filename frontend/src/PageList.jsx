988c3c54d93ea897f0a27fc4ff2246b0aa899341
Rachel Xie <z5141973@ad.unsw.edu.au>
2023-11-12 15:54:15 +1100

    Updated search logic; need to adjust date comparison

--- frontend/src/PageList.jsx
+++ frontend/src/PageList.jsx
@@ -18,9 +18,11 @@ export default function PageList () {
   const [errorModalMsg, setErrorModalMsg] = React.useState('');
   const [allListings, setAllListings] = React.useState([]);
   const [currentPage, setCurrentPage] = React.useState('landing')
+  const [publishedListings, setPublishedListings] = React.useState([]);
+  const [resultListings, setResultListings] = React.useState([]);
 
   const commonProps = { errorModalShow, setErrorModalShow, errorModalMsg, setErrorModalMsg, token, setToken, currentPage, setCurrentPage };
-  const listingProps = { allListings, setAllListings };
+  const listingProps = { allListings, setAllListings, publishedListings, setPublishedListings, resultListings, setResultListings };
 
   checkToken(setToken);
 
@@ -38,7 +40,7 @@ export default function PageList () {
       flexDirection: 'column'
     }}>
       {/* Header */}
-      <ResponsiveAppBar token={token} setToken={setToken} />
+      <ResponsiveAppBar {...commonProps} {...listingProps} />
       {/* Page */}
       <ErrorModal
         show={errorModalShow}
@@ -50,7 +52,7 @@ export default function PageList () {
         height: '100%'
       }}>
         <Routes>
-          <Route path="/" element={<LandingPage {...commonProps} />}></Route>
+          <Route path="/" element={<LandingPage {...commonProps} {...listingProps} />}></Route>
           <Route path="/register" element={<Register {...commonProps} />}></Route>
           <Route path="/login" element={<Login {...commonProps} />}></Route>
           <Route path="/my-hosted-listings" element={<HostedListings {...commonProps} { ...listingProps } />}></Route>
